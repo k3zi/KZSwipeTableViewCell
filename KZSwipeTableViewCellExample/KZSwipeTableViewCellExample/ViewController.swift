3ad7e52f82e663d6a83fe4beda0ee603dbcb2383
Kesi Maduka <me@kez.io>
2016-02-09 05:05:49 -0600

    Added Travis Fix

---
+++ KZSwipeTableViewCellExample/KZSwipeTableViewCellExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KZSwipeTableViewCellExample
+//
+//  Created by Kesi Maduka on 2/9/16.
+//  Copyright © 2016 Storm Edge Apps LLC. All rights reserved.
+//
+
+import UIKit
+import KZSwipeTableViewCell
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let cell = KZSwipeTableViewCell()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
